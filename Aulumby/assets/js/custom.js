85c1c45e084b951c639441859981a2adf7e2dccd
Frank Kirkemann <kirkemann35@gmail.com>
2020-01-29 20:15:28 +0100

    Initial commit

---
+++ Aulumby/assets/js/custom.js
@@ -0,0 +1,13 @@
+/* ----------------------------
+/*  Name: Aulum by
+    Author: Frank Kirkemann
+    Version: 2.0
+/* -------------------------- */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
